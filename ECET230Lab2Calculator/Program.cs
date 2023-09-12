63b0255750b09eab60a466f48396ae9384d864dd
sebastien <sebastien@robitaille.info>
2023-09-12 13:31:59 -0700

    Created basic console calculator app

--- ECET230Lab2Calculator/Program.cs
+++ ECET230Lab2Calculator/Program.cs
@@ -1,3 +1,72 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Calculator
+{
+    public static double doOperation(double num1, double num2, string op)
+    {
+        double result = double.NaN;
 
+        switch (op)
+        {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                if (num2 != 0)
+                {
+                    result = num1 / num2;
+                }
+                break;
+        }
+
+        return result;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var stopProgram = false;
+        Console.WriteLine("Welcome to the Calculator App by Sebastien Robitaille");
+        Console.WriteLine("-----------------------------------------------------");
+        while (!stopProgram)
+        {
+            Console.Write("Enter First Number: ");
+            string num1String = Console.ReadLine();
+            double num1;
+            while(!double.TryParse(num1String, out num1))
+            {
+                Console.Write("Input is not a number, try again: ");
+            }
+
+            Console.Write("Enter Second Number: ");
+            string num2String = Console.ReadLine();
+            double num2;
+            while (!double.TryParse(num2String, out num2))
+            {
+                Console.Write("Input is not a number, try again: ");
+            }
+
+            Console.WriteLine(@"Select from the following operation
+                                + - Add
+                                - - Subtract
+                                * - Multiply
+                                / - Devide");
+            Console.Write("Enter Operation: ");
+            string op = Console.ReadLine();
+            Console.WriteLine($"{num1} {op} {num2} = {Calculator.doOperation(num1, num2, op)}");
+            Console.WriteLine("------------------------------------------------------");
+            Console.Write("Enter x to exit program or any other key to continue: ");
+            string input = Console.ReadLine();
+
+            if(input == "x"){
+                stopProgram = true;
+            }
+        }
+    }
+}
